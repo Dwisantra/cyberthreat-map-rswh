8890aa7ef4194342eaaf4abf1bd5e3563e3e480b
dwisantra <itrswiradadihusada@gmail.com>
2026-08-06 11:11:39 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/cyber-map', function () {
-    return view('map');
+Route::get('/', function () {
+    return view('welcome');
 });
